7816efd1a37c34e73c28ca18a25651a4b0449ec2
Lucas Santana <lucas.santana@afrikatec.com.br>
2023-02-24 15:14:54 -0300

    24-02

--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -57,6 +57,7 @@ function ContributionsHandler(db) {
         }
         // Prevent more than 30% contributions
         //teste
+        //teste2222
         if (preTax + afterTax + roth > 30) {
             return res.render("contributions", {
                 updateError: "Contribution percentages cannot exceed 30 %",
